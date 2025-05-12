18732bf586cbd771e0c6fea5cd5984c6dfe63ee7
VmdJAVem <popeye801@outlook.com>
2025-05-12 18:15:43 -0500

    cOnFiG

---
+++ .config/nvim/lua/plugins/color.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- Ensure it loads first
+    opts = {
+      flavour = "mocha", -- Specify Mocha flavor
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        telescope = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            warnings = { "undercurl" },
+          },
+        },
+        -- Add other integrations as needed :cite[1]:cite[2]
+      },
+    },
+  },
+  -- Configure LazyVim to load Catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
