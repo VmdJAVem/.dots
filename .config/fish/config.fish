849c93134080105644346ba5a4833dae6a7fab45
VmdJAVem <popeye801@outlook.com>
2025-09-28 13:17:32 -0500

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,22 +4,19 @@
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias minecraft='java -jar ~/bin/SKlauncher-3.2.12.jar'
-alias unimatrix='unimatrix -n -s 96 -l o'
-alias neofetch='command neofetch --source ~/neofetch_ascii/aphex_twin'
 alias clock='tty-clock -c -s -b -f "%H:%M:%S" -C 3 -B "#6a2c8d" -t'
 alias cowsay='fortune | command cowsay -f stegosaurus'
 alias config='/usr/bin/git --git-dir=$HOME/poo --work-tree=$HOME'
-alias archfetch='neofetch --source ~/neofetch_ascii/arch.txt'
 alias cd='z'
 alias vim='nvim'
-alias myclock='~/bin/myclock'
 alias pdf='zpdf $(fzf)'
 alias gvim="nvim --listen /tmp/godothost"
 alias gayarch="hyfetch --preset bisexual --ascii-file /home/vmdjavem/neofetch_ascii/arch.txt"
 alias g++="g++ -std=c++23"
+alias cbonsai="cbonsai -l 50 -i -m "
 set EDITOR = nvim
 function fish_greeting
-    echo "hi :3"
+    echo "hola :3"
 end
 # Prompt customization
 function fish_prompt
